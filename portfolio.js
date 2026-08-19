499bdf8f2d007dd40e02b00c7cf9c8bd08d11415
Enrico <enricokuhndemoraespires@gmail.com>
2026-08-19 17:13:45 -0300

    alterações no layout

--- portfolio.js
+++ portfolio.js
@@ -1,12 +1,11 @@
 const header = document.getElementById("header");
 
 function handlescroll() {
-    if (window.scrollY > 24) {
-        header.classList.add ("is-scrolled");
-    }
-    else {
-        header.classList.remove ("is-scrolled");
-    }
+  if (window.scrollY > 24) {
+    header.classList.add("is-scrolled");
+  } else {
+    header.classList.remove("is-scrolled");
+  }
 }
 handlescroll();
-window.addEventListener("scroll", handlescroll, {passive: true});
+window.addEventListener("scroll", handlescroll, { passive: true });
